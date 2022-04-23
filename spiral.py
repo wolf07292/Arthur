1393ba915eb90959537ab7100babc7bae0324bae
wolf07292 <98645110+wolf07292@users.noreply.github.com>
2022-04-23 14:11:53 +0300

    Update spiral.py

--- spiral.py
+++ spiral.py
@@ -1,16 +1,20 @@
 import pygame as pg
+import random
+FPS = 
+clock = pg.time.Clock()
+win = pg.display.set_mode((500, 500))
 class Circle:
     def __init__(self,x,y,rad,col):
-        self.x = self
-        self.y = self
-        self.rad = self
-        self.col = self
+        self.x = x
+        self.y = y
+        self.rad = rad
+        self.col = col
         self.dir = 'right'
 
     def horizontal_movement(self):
         if self.dir == 'right':
             self.x += 1
-            if self.x > W:
+            if self.x > 500:
                 self.dir = 'left'
         else:
             self.x -= 1
@@ -18,4 +22,24 @@ class Circle:
                 self.dir = 'right'
 
     def draw(self, win):
-        pg.draw.circle(win, self.col, (self.x, self.y, ), self.rad)
+        pg.draw.circle(win, (self.col), (self.x, self.y), self.rad)
+
+
+list_circles = []
+for i in range(100):
+    list_circles.append(Circle(i * 10, i * 5, 30, random.choices(range(256), k=3)))
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+
+    win.fill((255, 255, 255))
+    for i in range(100):
+        list_circles[i].horizontal_movement()
+        list_circles[i].draw(win)
+
+
+
+    pg.display.update()
+    clock.tick(FPS)
