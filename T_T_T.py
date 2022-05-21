bae95fcfdc7ea43296e60594f96fbff949608040
wolf07292 <98645110+wolf07292@users.noreply.github.com>
2022-05-21 14:20:00 +0300

    Update T_T_T.py

--- T_T_T.py
+++ T_T_T.py
@@ -26,6 +26,8 @@ def is_end(board):
         if check_i_line(board, i):
             return 'line', i
     if check_main_diag(board):
+        return 'diag', 1
+    if check_secondary_diag(board):
         return 'diag', 2
     return None
 
@@ -79,6 +81,35 @@ class Board:
                     draw_cross(screen, x, y, self.size)
                 elif self.board[y][x] == -1:
                     draw_circle(screen, x, y, self.size)
+
+    def check_end(self):
+        is_end_info = is_end(self.board)
+        shift = self.W // 10
+        if is_end_info is not None:
+            type_end = is_end_info[0]
+            number = is_end_info[1]
+            print(is_end_info)
+            if type_end == 'col':
+                x0, y0 = (number + .5) * self.size, shift
+                x1, y1 = (number + .5) * self.size, self.size * 3 - shift
+            elif type_end == 'line':
+                x0, y0 = shift, (number + .5) * self.size
+                x1, y1 = 3 * self.size - shift, (number + .5) * self.size
+            elif type_end == 'diag':
+                if number == 1:
+                    x0, y0 = shift, shift
+                    x1, y1 = 3 * self.size - shift, 3 * self.size - shift
+                else:
+                    x0, y0 = 3 * self.size - shift, shift
+                    x1, y1 = shift, 3 * self.size - shift
+            print(x0)
+            pg.draw.line(screen, r, (x0, y0), (x1, y1), 10)
+            pg.display.update()
+            pg.time.delay(3000)
+            return True
+        else:
+            return False
+
 pg.init()
 screen = pg.display.set_mode((600,600))
 bo = Board(600,600,200)
